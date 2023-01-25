5c69bc638a81259b3a2e9c029a1d7d4e1387ff1f
David-Naray <modellingartsyt@gmail.com>
2023-01-25 22:20:46 +0000

    the media capabilty with /media/ url

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
+MEDIA_DIR=os.path.join(BASE_DIR, 'media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -122,3 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
+#The two variables tell Django where to look in your filesystem for media files
+#(MEDIA_ROOT) that have been uploaded and stored, and what URL to serve them from (MEDIA_URL)
+##ie http://localhost:8000/media/sample.pdf if we wanted to get sample.pdf
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
