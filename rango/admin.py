262633a58c4269acbb2943e272cf0ab771bdf320
David-Naray <modellingartsyt@gmail.com>
2023-01-26 23:57:06 +0000

    chap5 done

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageInline(admin.TabularInline):
+    model = Page
+    extra=4
+
+class PageAdmin(admin.ModelAdmin):
+    #inlines = [PageInline]
+    list_display = ('title','category', 'url')
+
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+
+admin.site.register(Page,PageAdmin)
+#admin.site.register(Page,PageInline)
