9b825537fcfaa206fc9b96ba0ca50041722b974e
David-Naray <modellingartsyt@gmail.com>
2023-01-27 03:23:19 +0000

    to the exercises of chap6

--- rango/admin.py
+++ rango/admin.py
@@ -9,9 +9,11 @@ class PageAdmin(admin.ModelAdmin):
     #inlines = [PageInline]
     list_display = ('title','category', 'url')
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category,CategoryAdmin)
 
 admin.site.register(Page,PageAdmin)
 #admin.site.register(Page,PageInline)
--- rango/models.py
+++ rango/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views=models.IntegerField(default=0)
     likes=models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)#blank=True is allowing blank entries,unique=True forces slug to be unique for each object
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
     class Meta:
         verbose_name_plural = 'Categories'
 
