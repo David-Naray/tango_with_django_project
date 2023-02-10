43dbb5f487acd85b3d18650eabe442594718cfab
David-Naray <modellingartsyt@gmail.com>
2023-02-10 07:32:59 +0000

    8 done, i think? the views was implied to be needing changes and i did 1 change, if there was more oof

--- rango/views.py
+++ rango/views.py
@@ -23,12 +23,14 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = Page_list
     return render(request, 'rango/index.html', context=context_dict)
+    #return render(request,reverse('index'), context=context_dict)
 
 
 def about(request):
 
     context_dict = {'boldmessage': 'this tutorial was put put together byDavid Naray'}
     return render(request, 'rango/about.html', context=context_dict)
+    ##return render(request, reverse('about'), context=context_dict)
 
 def show_category(request,category_name_slug):
     # Create a context dictionary which we can pass
@@ -56,6 +58,7 @@ def show_category(request,category_name_slug):
         context_dict['pages'] = None
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
+    #return render(request, reverse('show_category'), context=context_dict)
 
 def add_category(request):
     form = CategoryForm()
@@ -68,14 +71,18 @@ def add_category(request):
         form.save(commit=True)
         # Now that the category is saved, we could confirm this.
         # For now, just redirect the user back to the index view.
-        return redirect('/rango/')
+
+        #return redirect('/rango/')
+        return redirect(reverse('index'))
     else:
         # The supplied form contained errors -
         # just print them to the terminal.
         print(form.errors)
         # Will handle the bad form, new form, or no form supplied cases.
         # Render the form with error messages (if any).
+        
         return render(request, 'rango/add_category.html', {'form': form})
+        #return render(request, reverse('add_category'), {'form': form})
 
 def add_page(request, category_name_slug):
     try:
